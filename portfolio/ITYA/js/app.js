81a423742e872cf8e4f38fbe76c0adf95328a685
Tim Noakes-Tarrant <tim@noakes-tarrant.com>
2023-10-23 16:07:49 +0100

    changes

--- portfolio/ITYA/js/app.js
+++ portfolio/ITYA/js/app.js
@@ -1,12 +1,20 @@
-let navlinks = document.querySelectorAll("nav uk li a")
+let navlinks = document.querySelectorAll("header div nav ul li a")
 
 for (const link of navlinks) {
 	link.addEventListener("click", smoothScroll)
 }
 
  function smoothScroll(e) {
+	 console.log(e)
 	 e.preventDefault()
-	 const href = this.getAttribute("href") {
-		 behavior: "smooth"
-	 }
+	 const href = this.getAttribute("href") 
+	
+	document.querySelector(href).scrollIntoView({
+	
+	    behavior: "smooth",
+		block: "center"
+	  });
+	 
+	
  }
+
