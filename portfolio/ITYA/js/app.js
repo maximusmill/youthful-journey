48ccccfd483d43a8b16d245c47d8e64e95495236
Tim Noakes-Tarrant <tim@noakes-tarrant.com>
2023-10-31 21:45:36 +0000

    changes

--- portfolio/ITYA/js/app.js
+++ portfolio/ITYA/js/app.js
@@ -1,5 +1,5 @@
 let navlinks = document.querySelectorAll("header nav ul li a")
-let whatwecando = document.querySelector(".btn-cnt a")
+let whatwecando = document.querySelector("#home .lft-cnt a")
 let wwcdelement = document.getElementById("services")
 
 
@@ -19,3 +19,16 @@ for (const link of navlinks) {
 	  }); 
  }
 
+const isMob360 = window.matchMedia('(min-width: 360px)'); 
+  
+ const handleResize = e => { 
+   if (e.matches) { 
+	 console.log('viewport above 360px!'); 
+   } 
+ }; 
+  
+ // handles our media query as/when it changes 
+ isMob360.addEventListener('change', e => handleResize(e)); 
+  
+ // instantiates the media query at load-time 
+ handleResize(isMob360) 
