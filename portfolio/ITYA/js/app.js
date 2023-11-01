cf025617d9de4e8321e27eb29e8beccb786d2d8c
Tim Noakes-Tarrant <tim@noakes-tarrant.com>
2023-11-01 18:50:31 +0000

    changes

--- portfolio/ITYA/js/app.js
+++ portfolio/ITYA/js/app.js
@@ -1,6 +1,8 @@
 let navlinks = document.querySelectorAll("header nav ul li a")
 let whatwecando = document.querySelector("#home .lft-cnt a")
 let wwcdelement = document.getElementById("services")
+let brandscontainer = document.querySelector("#brands")
+const fragment = document.createDocumentFragment()
 
 
 whatwecando.addEventListener('click',smoothScroll)
@@ -12,21 +14,50 @@ for (const link of navlinks) {
  function smoothScroll(e) {
 	 e.preventDefault()
 	 const href = this.getAttribute("href") 
-	
-	document.querySelector(href).scrollIntoView({
+		document.querySelector(href).scrollIntoView({
 		behavior: "smooth",
 		block: "center"
 	  }); 
  }
 
+
 const isMob360 = window.matchMedia('(min-width: 360px)'); 
-  
- const handleResize = e => { 
+  console.log(MediaQueryList)
+  console.log(isMob360)
+  const handleResize = e => { 
+	 console.log(e)
+	
    if (e.matches) { 
+	   
+	  console.log(e.matches) 
 	 console.log('viewport above 360px!'); 
-   } 
+	  let div = document.createElement('div')
+ 		 div.classList.add('brand-cnt')
+	 	 div.innerHTML = `<img src="img/intuneicn.svg" width="111" height="25" alt="Micorsoft Intune Logo">`
+	 	 fragment.appendChild(div)
+	 	 brandscontainer.appendChild(fragment)
+
+	 let div1 = document.createElement('div')
+		 div1.classList.add('brand-cnt')
+		 div1.innerHTML = `<img src="img/azureicn.svg" width="111" height="25" alt="Micorsoft Azure Logo">`
+		 fragment.appendChild(div1)
+   		 brandscontainer.appendChild(fragment)
+   } else {
+	   
+	   const divreset = document.querySelector('#brands')
+	   
+	   divreset.innerHTML = `
+		   <div class="brand-cnt">
+			   <img src="img/365icn.svg" width="111" height="25" alt="Microsoft 365 Logo">
+		   </div>
+		   <div class="brand-cnt">
+			   <img src="img/win11icn.svg" width="111" height="25" alt="Windows 11 Logo">
+		   </div>	
+	  `
+	   
+   }
  }; 
-  
+  console.log(brandscontainer)
  // handles our media query as/when it changes 
  isMob360.addEventListener('change', e => handleResize(e)); 
   
