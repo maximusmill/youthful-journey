5af7adb6005613fe4ce9e24ef937d22b1649cb6f
Tim Noakes-Tarrant <tim@noakes-tarrant.com>
2023-11-09 16:23:12 +0000

    changes

--- portfolio/ITYA/js/app.js
+++ portfolio/ITYA/js/app.js
@@ -1,9 +1,5 @@
 let navlinks = document.querySelectorAll("header nav ul li a")
 let whatwecando = document.querySelector("#home .lft-cnt a")
-let wwcdelement = document.getElementById("services")
-let brandscontainer = document.querySelector("#brands")
-let homecontainer = document.querySelector("#home")
-const fragment = document.createDocumentFragment()
 
 
 whatwecando.addEventListener('click',smoothScroll)
@@ -17,6 +13,6 @@ for (const link of navlinks) {
 	 const href = this.getAttribute("href") 
 		document.querySelector(href).scrollIntoView({
 		behavior: "smooth",
-		block: "center"
+		block: "start"
 	  }); 
  }
